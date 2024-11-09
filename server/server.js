49cd1f51e893de7db7da25a1e7772a7f5d912571
Daniel Bucket <danieljbucket@gmail.com>
2024-11-09 13:32:24 -0700

    webpack.production.config is now compiling as expected

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const express = require('express')
 const cors = require('cors')
-const webpack = require('webpack')
-const middleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-const config = require('../webpack.dev.js')
-const compiler = webpack(config)
+// const webpack = require('webpack')
+// const middleware = require('webpack-dev-middleware')
+// const webpackHotMiddleware = require('webpack-hot-middleware')
+// const config = require('../webpack.dev.js')
+// const compiler = webpack(config)
 
 const app = express()
 const router = require('./router.js')
@@ -15,10 +15,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
 
-app.use(middleware(compiler, {
-  publicPath: config.output.publicPath,
-}))
-app.use(webpackHotMiddleware(compiler))
+// app.use(middleware(compiler, {
+//   publicPath: config.output.publicPath,
+// }))
+// app.use(webpackHotMiddleware(compiler))
+
 app.use(express.static(path.resolve(__dirname, '../dist')))
 
 app.use('/', (req,res) => {
